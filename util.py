2de28cf3a779ea2c6dccc0546c382c850825ec56
Daniel Navarro <dsnavarro361@gmail.com>
2018-07-05 16:48:58 -0500

    Final Updates
    Regex search function has been completed and tested

--- util.py
+++ util.py
@@ -23,41 +23,64 @@ def date_format(date):
         try:
             datetime.datetime.strptime(task_date, "%m/%d/%Y")
             formatted = False
+            clear()
         except ValueError:
+            clear()
             task_date = input(
                 "Sorry. That is not a valid date. Please enter a date"
                 "in the specified format: \n>")
+
     return task_date
+
 
 
 def time_format(time):
     """Verifies that the user has entered the time spent in a rounded
-    minutes format"""
+    minutes format
+    """
     task_time = time
     formatted = True
     while formatted:
         try:
             int(task_time)
             formatted = False
+            clear()
         except ValueError:
+            clear()
             task_time = input("Please submit the time in rounded minutes: \n>")
     return task_time
 
 
 def search_csv(search_criteria):
     """Searches the CSV for the user provided criteria, then calls the
-    display_search_results function once it is found"""
+    display_search_results function once it is found
+    """
     with open('work_log.csv', 'r') as file:
         csvreader = csv.reader(file, delimiter=',')
         rows = list(csvreader)
         for row in rows:
             if search_criteria in row:
                 display_search_results(row)
+            else:
+                input("Your search did not yield any results. "
+                      "Please press Enter to return to the main menu")
+                clear()
 
 
-def reg_csv_search(pattern):
-    try:
-        re.search(pattern)
+def reg_csv_search(arg):
+    """Uses a regex pattern to search the CSV for the users specified
+    criteria
+    """
+    with open('work_log.csv', 'r') as file:
+        csvreader = csv.reader(file, delimiter=',')
+        rows = list(csvreader)
+        for row in rows:
+            if re.search(arg, str(row)):
+                display_search_results(row)
+            else:
+                input('Your search did not yield any results. Please press '
+                      'Enter to return to the main menu')
+                clear()
 
 
 def display_search_results(results):
@@ -69,3 +92,4 @@ def display_search_results(results):
     print('Notes : {}'.format(results[3]))
     input("Press Enter to cycle through entries or "
           "return to the main menu\n")
+    clear()
--- work_log.py
+++ work_log.py
@@ -10,7 +10,8 @@ class Worklog:
 
     def main_menu(self):
         """This brings up the main menu of the program. Allowing
-        users to pick which task they would like to do"""
+        users to pick which task they would like to do
+        """
 
         menu_options = {'a': self.add_entry, 'b': self.search_entry,
                         'c': self.quit_program}
@@ -25,32 +26,43 @@ class Worklog:
 
             try:
                 choice = menu_options[menu_choice]
+                clear()
 
             except KeyError:
                 input("That is not a valid choice. Please try again ")
                 self.main_menu()
             else:
                 choice()
+                clear()
 
     def add_entry(self):
         """Writes user input to a csv"""
         entry_date = date_format(input("Date of the task"
                                        "\nPlease use the "
-                                       "DD/MM/YYYY: ")).strip()
+                                       "DD/MM/YYYY:\n> ")).strip()
+        clear()
 
-        entry_title = input("Title of the task: ").strip().upper()
+        entry_title = input("Title of the task:\n> ").strip().upper()
+        clear()
 
         entry_time_spent = time_format(input("Time spent "
-                                             "(rounded in minutes): "))
+                                             "(rounded in minutes):\n> "))
+        clear()
 
-        entry_notes = input("Notes (Optional): ").strip().upper()
+        entry_notes = input("Notes (Optional):\n> ").strip().upper()
+        clear()
 
         with open('work_log.csv', 'a') as file:
             file.write(entry_date + ',' + entry_title + ',' +
                        entry_time_spent + ',' + entry_notes + '\n')
         input("Your entry has been added. Press enter to return to the menu\n")
+        clear()
 
     def search_entry(self):
+        """This prompts the user with option of searching existing entries
+        and gives the user four different options as to what to specifically
+        search for
+        """
 
         search_options = {'a': self.search_date, 'b': self.search_time_spent,
                           'c': self.exact_search, 'd': self.search_regex,
@@ -64,32 +76,49 @@ class Worklog:
                               "\n> ")
         try:
             choice = search_options[search_choice]
+            clear()
 
         except KeyError:
             input("That is not a valid choice. Please try again \n>")
             self.search_entry()
+            clear()
         else:
             choice()
+            clear()
+
 
     def search_date(self):
+        """This function takes the user's argument and passes it to the
+        search_csv function which then displays it to the user
+        """
         desired_date = date_format(input("What date would you like "
                                          "to search for? \n>"))
         search_csv(desired_date)
 
     def search_time_spent(self):
+        """This function takes the user's argument and passes it to the
+        search_csv function which then displays it to the user
+        """
         spent_time = time_format(input('How much time did you spend on'
                                        ' this job task? \n>'))
         search_csv(spent_time)
 
     def exact_search(self):
+        """This function takes the user's argument and passes it to the
+        search_csv function which then displays it to the user
+        """
         exactly = input('What is the exact task you are looking for? \n>')
 
         search_csv(exactly.upper())
 
     def search_regex(self):
+        """This function takes the user's inputted regex pattern and passes
+        it to the reg_csv_search funtion where it is then displayed to the
+        user
+        """
         pattern = input("Please enter the REGEX pattern you would like "
                         "to use to search for the log: \n>")
-        re_csv_search(pattern)
+        reg_csv_search(pattern)
 
 
 if __name__ == '__main__':
